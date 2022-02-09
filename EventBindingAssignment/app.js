eb6e4cf8e7c72b46f42f02438bf5aaa1b9a3bcc5
Afuwape Idris <afuidris@gmail.com>
2022-02-09 10:27:02 +0100

    Added computed properties

--- EventBindingAssignment/app.js
+++ EventBindingAssignment/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             output: "",
             completeText: "",
+            num: 0,
         };
     },
 
@@ -10,11 +11,32 @@ const app = Vue.createApp({
         showAlert() {
             alert("This is my event class");
         },
+
         updateInput(event) {
             this.output = event.target.value;
         },
+
         updateCompleteText(event) {
             this.completeText = event.target.value;
+        },
+
+        showText() {
+            console.log("Runnnin");
+            return this.completeText;
+        },
+
+        increaseNumber() {
+            this.num += 5;
+        },
+
+        decreaseNumber() {
+            this.num -= 5;
+        },
+    },
+
+    computed: {
+        showCompleteText() {
+            return this.completeText;
         },
     },
 });
