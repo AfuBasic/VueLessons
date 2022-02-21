50eb8ad78a38a692f923d92df831f0666f884cb4
Afuwape Idris <afuidris@gmail.com>
2022-02-21 01:48:18 +0100

    Monster fight game

---
+++ Module3/MonsterSlayer/app.js
@@ -0,0 +1,64 @@
+const getRamdonValue = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+const app = Vue.createApp({
+    data() {
+        return {
+            health: {
+                player: 100,
+                monster: 100,
+            },
+            currentRound: 0,
+        };
+    },
+
+    computed: {
+        playerBar() {
+            return { width: this.health.player + "%" };
+        },
+
+        monsterBar() {
+            return { width: this.health.monster + "%" };
+        },
+
+        specialAttackAvailable() {
+            if (this.currentRound === 0) return true;
+            return this.currentRound % 3 !== 0;
+        },
+    },
+
+    methods: {
+        attackMonster() {
+            const attackValue = getRamdonValue(5, 15);
+            this.health.monster -= attackValue;
+            this.currentRound++;
+            this.attackPlayer();
+        },
+
+        attackPlayer() {
+            const attackValue = getRamdonValue(8, 20);
+            this.health.player -= attackValue;
+        },
+
+        specialAttack() {
+            this.currentRound++;
+            const attackValue = getRamdonValue(10, 25);
+            this.health.monster -= attackValue;
+            this.attackPlayer();
+        },
+
+        healPlayer() {
+            const healValue = getRamdonValue(8, 20);
+            if (this.health.player + healValue > 100) {
+                this.health.player = 100;
+            } else {
+                this.health.player += healValue;
+            }
+
+            this.attackPlayer();
+        },
+    },
+});
+
+app.mount("#game");
