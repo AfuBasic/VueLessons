80d1607a7e58662b7a3e8bfd3c30c74345ee5cf0
Afuwape Idris <afuidris@gmail.com>
2022-03-04 02:24:23 +0100

    first look at components in vue

--- FriendList/app.js
+++ FriendList/app.js
@@ -18,4 +18,35 @@ const app = Vue.createApp({
     },
 });
 
+app.component("friend-contact", {
+    template: `
+	<li>
+		<h2>{{ friend.name }}</h2>
+		<button @click="toggleDetails">Show Details</button>
+		<ul v-if="friendVisible">
+			<li><strong>Phone:</strong> {{ friend.phone }}</li>
+			<li><strong>Email:</strong> {{ friend.email }}</li>
+		</ul>
+	</li>
+	`,
+
+    data() {
+        return {
+            friendVisible: false,
+            friend: {
+                id: "tunde",
+                name: "Afuwape Tunde",
+                phone: "08078767676",
+                email: "email@owner.com",
+            },
+        };
+    },
+
+    methods: {
+        toggleDetails() {
+            this.friendVisible = !this.friendVisible;
+        },
+    },
+});
+
 app.mount("#app");
