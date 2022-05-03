0e4ba5ac7cc1e4a9f58bbf5b659624736adbddd5
Afuwape Idris <afuidris@gmail.com>
2022-05-03 13:42:04 +0100

    A lot of changes that I did not track

---
+++ adv-cmp/src/components/BaseCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <slot name="topSlot"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["content"],
+};
+</script>
---
+++ adv-cmp/src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <base-card>
+      <template v-slot:topSlot>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <p>{{ infoText }}</p>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+export default {
+  components: { BaseCard },
+  props: ["fullName", "infoText", "role"],
+};
+</script>
+
+<style>
+section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
