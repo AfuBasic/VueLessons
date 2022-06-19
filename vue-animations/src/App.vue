b6da312a47f08bd643f37ebcc56afd70d44a1369
Afuwape Idris <afuidris@gmail.com>
2022-06-19 15:25:30 +0100

    vue animation property

--- vue-animations/src/App.vue
+++ vue-animations/src/App.vue
@@ -3,6 +3,13 @@
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
+  <div class="container">
+    <transition name="para">
+      <p v-if="paraIsVisible">This is only sometimes visible</p>
+    </transition>
+    <button @click="togglePara">Toggle</button>
+  </div>
+
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -15,7 +22,11 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false, animatedBlock: false };
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+    };
   },
   methods: {
     showDialog() {
@@ -26,6 +37,10 @@ export default {
     },
     animateBlock() {
       this.animatedBlock = true;
+    },
+
+    togglePara() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
   },
 };
@@ -77,6 +92,34 @@ button:active {
 .animate {
   /* transform: translateX(-50px); */
   animation: slide-fade 0.5s ease-out forwards;
+}
+
+.para-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.para-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.para-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.para-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.para-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.para-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
 @keyframes slide-fade {
