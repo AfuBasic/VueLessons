3cc1bc9eeb8e0a089106b5c8d922ce439dece41c
Afuwape Idris <afuidris@gmail.com>
2022-06-20 04:13:40 +0100

    Animating List Items with Vue

---
+++ vue-animations/src/components/UsersList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <div>
+    <input type="text" v-model="user" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Tunde', 'Basic', 'Afuwape', 'Idris', 'Ayinde'],
+      user: '',
+    };
+  },
+
+  methods: {
+    addUser() {
+      this.users.unshift(this.user);
+    },
+    removeUser(user) {
+      this.users = this.users.filter((usr) => usr !== user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #e1e1e1;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
