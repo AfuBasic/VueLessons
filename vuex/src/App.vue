3b514bc5ecb2d972256c37a6697dcbd701615901
Afuwape Idris <afuidris@gmail.com>
2022-06-25 02:10:10 +0100

    Combining the usage of State, mutations, getters, actions, mapGetters and mapAction to build a simple auth feature

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="userIsAuthenticated">
     <base-counter></base-counter>
     <change-counter></change-counter>
     <favorite-value></favorite-value>
   </base-container>
+  <base-container title="Auth"> <user-auth></user-auth> </base-container>
 </template>
 
 <script>
@@ -11,6 +12,9 @@ import BaseContainer from './components/BaseContainer.vue';
 import BaseCounter from './components/BaseCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
+
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
@@ -18,6 +22,11 @@ export default {
     BaseCounter,
     ChangeCounter,
     FavoriteValue,
+    UserAuth,
+  },
+
+  computed: {
+    ...mapGetters(['userIsAuthenticated']),
   },
 };
 </script>
---
+++ vuex/src/components/UserAuth.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="login" v-if="!userIsAuthenticated">Login</button>
+  <button @click="logout" v-else>Logout</button>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions({
+      login: 'login',
+      logout: 'logout',
+    }),
+  },
+
+  computed: {
+    ...mapGetters(['userIsAuthenticated']),
+  },
+};
+</script>
