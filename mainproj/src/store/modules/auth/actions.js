4a4393bfd7c57d69b89da8d61f2ec0637e386998
Afuwape Idris <afuidris@gmail.com>
2022-07-12 07:41:27 +0100

    Added an auto logout feature

--- mainproj/src/store/modules/auth/actions.js
+++ mainproj/src/store/modules/auth/actions.js
@@ -1,5 +1,8 @@
 import { fireBaseKey } from '../../../utils/constants';
 import { errorHandler } from '../../../utils/helpers';
+import router from '../../../router';
+
+let timer;
 
 export default {
     async login(context, payload) {
@@ -36,18 +39,24 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.log(responseData);
             const error = errorHandler(responseData.error.message);
             throw error;
         }
 
+        const tokenExpiresIn = responseData.expiresIn * 1000;
+        const tokenExpiration = new Date().getTime() + tokenExpiresIn;
+
         localStorage.setItem('_token', responseData.idToken);
         localStorage.setItem('_userId', responseData.localId);
+        localStorage.setItem('_tokenExpiresIn', tokenExpiration);
+
+        timer = setTimeout(() => {
+            context.dispatch('logout');
+        }, tokenExpiresIn);
 
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn,
         });
     },
 
@@ -55,20 +64,37 @@ export default {
         const token = localStorage.getItem('_token');
         const userId = localStorage.getItem('_userId');
 
+        //Ensure that the user still has a valid token
+        const tokenExpiration = localStorage.getItem('_tokenExpiresIn');
+        const expiresIn = +tokenExpiration - new Date().getTime();
+
+        if (expiresIn < 0) {
+            return;
+        }
+
+        timer = setTimeout(() => {
+            context.dispatch('logout');
+        }, expiresIn);
+
         if (token && userId) {
             context.commit('setUser', {
                 token,
                 userId,
-                tokenExpiration: null,
             });
         }
     },
 
     logout(context) {
+        localStorage.removeItem('_token');
+        localStorage.removeItem('_userId');
+        localStorage.removeItem('_tokenExpiresIn');
+
+        clearTimeout(timer);
         context.commit('setUser', {
             token: null,
             userId: null,
-            tokenExpiration: null,
         });
+
+        router.replace('/auth');
     },
 };
